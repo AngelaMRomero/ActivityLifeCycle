16e31b6677b96882dc5765faabc9d84d644cef1d
aplimovil <fomarti@unicauca.edu.co>
2019-03-22 11:13:54 -0500

    Initial setup

---
+++ app/src/main/java/aplimovil/com/activitylifecycle/StateTestActivity.java
@@ -0,0 +1,12 @@
+package aplimovil.com.activitylifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StateTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
